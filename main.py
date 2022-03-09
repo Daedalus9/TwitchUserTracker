7ed18bb7aec2ece5e28c0aaa106b809b30a44832
Daedalus9 <longo.luca94@hotmail.it>
2022-03-09 08:23:33 +0100

    refactor: added "search.py" module

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import requests
-us = open("list.txt", "r")
-user=input("Inserisci il nick: ")
-for line in us:
-    url = "https://tmi.twitch.tv/group/user/"+line.lower()[:-1]+"/chatters"
-    r=requests.get(url).json()
-    chat=r.get('chatters')['viewers']
-    if(user in chat):
-            print(user + " sta guardando " + line[:-1])
-us.close()
+from module.search import search
+
+def main():
+    search()
+
+if __name__ == '__main__':
+    main()
