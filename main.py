a454e765d42dd90645a6ce2187bf251db024d1dd
Daedalus9 <50534107+Daedalus9@users.noreply.github.com>
2022-03-13 09:52:23 +0100

    feat: nickname can be parsed as arg

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from module.search import search
+import sys
 
 def main():
-    search()
+    args = sys.argv[1:]
+    if len(args)>1 and args[0] == '--nick':
+        nickname = args[1]
+        search(nickname)
 
 if __name__ == '__main__':
     main()
